4ac73e70527e32a57682e1894bd1e28e2dd65f42
Nicky Hajal <nhajal@gmail.com>
2017-10-28 14:34:00 -0500

    tickets

--- app/models/tickets.js
+++ app/models/tickets.js
@@ -1,7 +1,9 @@
 const Shelf = require('./shelf');
 const Bookshelf = require('bookshelf');
 const Q = require('q');
-const [User, Users] = require('./users');
+const __users = require('./users');
+const User = __users[0];
+const Users = __users[1];
 
 const Ticket = Shelf.Model.extend({
   tableName: 'tickets',
