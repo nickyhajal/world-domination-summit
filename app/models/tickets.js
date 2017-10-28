ab16f0bcd3918fb47fdc2955f34c70d73c986fe5
Nicky Hajal <nhajal@gmail.com>
2017-10-28 14:30:49 -0500

    tickets

---
+++ app/models/tickets.js
@@ -0,0 +1,34 @@
+const Shelf = require('./shelf');
+const Bookshelf = require('bookshelf');
+const Q = require('q');
+const [User, Users] = require('./users');
+
+const Ticket = Shelf.Model.extend({
+  tableName: 'tickets',
+  idAttribute: 'ticket_id',
+  hasTimestamps: true,
+});
+
+const Tickets = Shelf.Collection.extend({
+  model: Ticket,
+  async getUser() {
+    const user = await Users.forge({ user_id: this.user_id }).fetch();
+    return user;
+  },
+  async cancelTicket() {
+    const yr = 'attending' + process.yr;
+    const originalStatus = this.get('status');
+    const saveResult = await this.set({ status: 'canceled' }).save();
+    if (originalStatus === 'active') {
+      const user = await this.getUser();
+      const userRes = await user.set({ [yr]: '-1' }).save();
+      user.removeFromList('WDS ' + process.year + ' Attendees').then(() => {
+        user.addToList('WDS ' + process.year + ' Canceled');
+      });
+      return [user, this];
+    }
+    return [null, this];
+  },
+});
+
+module.exports = [Ticket, Tickets];
