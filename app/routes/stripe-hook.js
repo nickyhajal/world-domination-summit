4d3f731a31fdc91cf4e8874b2dc54a2635149f91
Nicky Hajal <nhajal@gmail.com>
2018-10-05 12:26:05 -0500

    webbhook

--- app/routes/stripe-hook.js
+++ app/routes/stripe-hook.js
@@ -4,8 +4,6 @@ const { processEvent } = require('./stripe-hook-functions');
 
 const routes = app => {
   return app.all('/stripewh', (req, res) => {
-    res.send('hey');
-    return;
     try {
       let sig = req.headers['stripe-signature'];
       let ev = stripe.webhooks.constructEvent(
